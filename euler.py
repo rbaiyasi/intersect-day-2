f025d7809e5240464a17c71835deb38ee88c0eff
rbaiyasi <rbaiyasi@gmail.com>
2023-07-11 16:24:34 -0400

    Update euler.py documentation.
    Add documentation to deriv method

--- euler.py
+++ euler.py
@@ -1,7 +1,16 @@
 class EulersMethod(self):
+   '''Approximate step in function using Euler's method.'''
    def deriv(self, x, y):
-       return y**2 + y*x + x**3
+      '''Return derivative (?) of... y?
+      Inputs
+      ------
+      x: float
+         Independent variable
+      y: float
+         Current value of function.
+      '''
+      return y**2 + y*x + x**3
    def approx(self, y, x, h):
-       y_j = y + h*self.deriv(x, y)
-       x_j = x + h
-       return y_j, x_j
+      y_j = y + h*self.deriv(x, y)
+      x_j = x + h
+      return y_j, x_j
